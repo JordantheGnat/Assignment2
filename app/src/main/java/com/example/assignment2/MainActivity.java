ff4fe17d8614869b7a4655368f96e78ec97bbd40
JordantheGnat <jordanmandrson@gmail.com>
2023-10-11 21:01:08 -0500

    created two containers in landscape mode.

--- app/src/main/java/com/example/assignment2/MainActivity.java
+++ app/src/main/java/com/example/assignment2/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.assignment2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    FragmentManager fg;
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fg=getSupportFragmentManager();
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            fg.beginTransaction().replace(R.id.topContainer,
+                    new TickerListFragment()).replace(R.id.bottomContainer, new InfoWebFragment()).commit();
+                    //new ColorPickFragment()).replace(R.id.bottomContainer, new HistoryFragment()).commit();
+        }
     }
 }
